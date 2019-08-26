750c22082f499c8b9e8dae155928d8bb5972647c
Toni-Ty <antonique2479@yahoo.com>
2019-08-26 01:51:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,6 +43,19 @@ def self.find_by_name(name)
   end.first
 end
 
+def self.find_by_id(id)
+  sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE id = ?
+    LIMIT 1
+  SQL
+
+  DB[:conn].execute(sql, id).map do |row|
+    self.new_from_db(row)
+  end.first
+end
+  
 
 
 
